f02fb40b96192f8ba680890407ae593422aad689
leewooseong <sitpo96@naver.com>
2022-04-18 09:35:18 +0900

    Input 기능 완료

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,23 +1,39 @@
 import styled from "styled-components";
+import React, { useRef, useState } from "react";
 const StyledForm = styled.form`
-    display: flex;
+    display: inline-flex;
     flex-direction: column;
     justify-content: center;
     margin: 30px;
 `;
 const InputWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+
     position: relative;
-    margin-bottom: ${(props) =>
-        props.className === "email-wrapper" ? "25px" : "0px"};
-    & img {
+    margin-bottom: 5px;
+    & button {
         position: absolute;
-        width: 15px;
-        left: 235px;
-        bottom: 10px;
+        width: 20px;
+        height: 20px;
+        right: 12px;
+        top: 35px;
+        padding: 0;
+        border: none;
+        background-color: transparent;
+    }
+    & img {
+        position: ${(props) =>
+            props.className === "email-wrapper" ? "absolute" : "inline-block"};
+        width: ${(props) =>
+            props.className === "email-wrapper" ? "15px" : "20px"};
+        right: ${(props) =>
+            props.className === "email-wrapper" ? "15px" : "0"};
+        top: ${(props) => (props.className === "email-wrapper" ? "35px" : "0")};
     }
 `;
 const StyledLabel = styled.label`
-    display: block;
+    display: inline-block;
     margin: 5px;
     font-size: 12px;
     color: gray;
@@ -36,27 +52,72 @@ const StyledInput = styled.input`
     &:active {
         outline: 1px solid #999;
     }
-    /* input box안에 아이콘 삽입 */
-    &:after {
-        display: inline-block;
-        content: "그림";
-        position: absolute;
-        right: 100px;
-        height: 10px;
-        width: 10px;
-        color: #000;
-        font-size: 10px;
-    }
 `;
 const StyledP = styled.p`
+    color: #d73333;
+    font-size: 10px;
+`;
+const BlindP = styled.p`
     position: absolute;
     width: 1px;
     height: 1px;
     clip: rect(0 0 0 0);
     overflow: hidden;
 `;
-
 const Input = () => {
+    // email 관련 Hook
+    const [emailValid, setEmailValid] = useState(false);
+    const [showAlert, setShowAlert] = useState(false);
+
+    // password 관련 Hook
+    const [showPW, setShowPW] = useState(false);
+
+    // email 유효성 검사 함수
+    const validateEmail = (input) => {
+        const emailRule =
+            /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+        return emailRule.test(input);
+    };
+
+    // email input DOM의 이벤트 핸들러
+    const handleChange = (e) => {
+        // js에서는 eventTarget.value였나.. 찾아보기
+
+        // 1. 이메일이 비었다면 알림 끄기
+        // 2. 이메일이 유효해졌다면 체크버튼 활성화
+        // 3. 입력에 의해 이메일이 유효하지 않아지면 체크버튼 비활성화
+        if (e.target.value === "") {
+            setShowAlert(false);
+        } else if (validateEmail(e.target.value)) {
+            setEmailValid(true);
+            setShowAlert(false);
+        } else if (!validateEmail(e.target.value)) {
+            setEmailValid(false);
+        }
+    };
+
+    const handleBlur = (e) => {
+        // emailValid가 false일 때 포커스 아웃된 경우
+        if (
+            e.target.value !== "" &&
+            !emailValid &&
+            e.currentTarget === e.target
+        ) {
+            setShowAlert(true);
+        }
+        // emailValid가 true일 때 포커스 아웃된 경우
+        else if (emailValid && e.currentTarget === e.target) {
+            setShowAlert(false);
+        }
+    };
+
+    const handlePWImgClick = (e) => {
+        setShowPW((preState) => {
+            return !preState;
+        });
+    };
+
+    // rendering 함수
     return (
         <StyledForm className="input_cont">
             <InputWrapper className="email-wrapper">
@@ -67,22 +128,58 @@ const Input = () => {
                     name="email"
                     id="email-inp"
                     placeholder="E-mail"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
                 />
-                <img src="images/check_active.svg" alt="" />
-                <StyledP className="blind">Invalid e-mail address.</StyledP>
+                <img
+                    src={
+                        emailValid
+                            ? "images/check_active.svg"
+                            : "images/check_unactive.svg"
+                    }
+                    alt=""
+                />
             </InputWrapper>
+            {showAlert ? (
+                <StyledP>Invalid e-mail address.</StyledP>
+            ) : (
+                <BlindP>Invalid e-mail address.</BlindP>
+            )}
             <InputWrapper className="pw-wrapper">
                 <StyledLabel htmlFor="password-inp">Password</StyledLabel>
                 <StyledInput
-                    type="password"
+                    type={showPW ? "text" : "password"}
                     name="password"
                     id="password-inp"
                     placeholder="Password"
                 />
-                <img src="images/pw-show.svg" alt="" />
+                <button type="button" onClick={handlePWImgClick}>
+                    <img
+                        src={
+                            showPW
+                                ? "images/pw-show.svg"
+                                : "images/pw-unshow.svg"
+                        }
+                        alt="비밀번호 표시 여부 이미지"
+                    />
+                </button>
             </InputWrapper>
         </StyledForm>
     );
 };
 
 export default Input;
+
+// email 요구 사항
+// 1. validate === true 면 체크 버튼이 활성화
+// 2. validate === false인 상태로 다음 탭으로 넘어가면 알림 활성화
+// 3. 메일에 입력이 없으면 알림 비활성화
+
+// password 요구 사항
+// 1. 눈 모양 버튼을 클릭하지 않으면 비밀번호 형식으로 표시
+// 2. 눈 모양 버튼을 클릭해서 활성화가되면 입력된 비밀번호 원문이 보이도록 표시
+
+// PureComonent 사용하는 이유에 대해서 한번 생각해보기
+// 추측
+// - setState 함수들이 호출되면 무조건 다시 렌더링되는데 이때 state변화가 없어도 렌더링되기 때문인가?
+// - 렌더링되는 컴포넌트의 하위 컴포넌트들이 강제적으로 렌더링되어서인가?
