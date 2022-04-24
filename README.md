075af531835bf09a2c9d3a93d6c8b092b14d7c10
leewooseong <sitpo96@naver.com>
2022-04-24 17:24:01 +0900

    Input.js 코드 정리 및 README 작성

--- README.md
+++ README.md
@@ -10,7 +10,39 @@
 
 ### 4-1. 구현 방법
 
+**HTML**
+
+-   form tag 안에 div tag 2개가 들어있는 방식입니다.
+-   각 div tag는 email관련 input 처리하는 것 1개, password 관련 input 처리하는 것 1개로 구성되어 있습니다.
+
+**CSS**
+
+-   blind를 이용하여 에러가 나지 않은 email의 경우 경고 알림을 표시하지 않습니다.
+
+**JavaScript**
+
+-   state
+    -   emailValid : 이메일 유효성 결과를 boolean 값으로 표협합니다.
+    -   showAlert : 경로 알림을 띄울지 말지를 boolean 값으로 표현합니다.
+    -   showPW : pw를 text로 표시할 지 여부를 boolean 값으로 표현합니다.
+-   Email 유효성 검사
+    -   email-wrapper class 내의 input 값이 변화하면 입력된 값을 이용해 email이 유효한지 판단합니다.
+    -   유효성 검사는 정규식을 이용하여 email 조건을 만족하는 지 검사합니다.
+    -   단 입력된 값이 `""`라면 이메일 유효성 검사를 하지 않습니다.
+    -   입력된 값이 유효성 검사를 통과한다면 showAlert 값을 false로 바꾸고 emailValid를 true로 바꿔줍니다. 이에 따라 email-wrapper class 내의 img가 활성화된 이미지로 바뀝니다.
+    -   입력된 값이 유효성 검사를 통과하지 못한다면 emailValid를 false로 바꿔줍니다. 이에 따라 email-wrapper class 내의 img가 비활성화된 이미졸 바뀝니다.
+    -   emailValid 값이 false이더라도 focus가 해제되지 않는한 showAlert 값이 true로 바뀌지 않습니다.
+-   경고 알림 표시하기
+    -   showAlert state 값에 따라 알림이 표시됩니다.
+    -   input의 입력에 의해 emailValid가 false인 상태에서 focus가 해제되면 showAlert 값이 true로 바뀌고 경고 알림이 표시됩니다.
+    -   input의 입력에 의해 emailValid가 true가되면 focus 여부와 상관없이 showAlert 값이 false로 바뀌고 경고 알림이 사라집니다.
+-   비밀번호 표시하기
+    -   비밀번호란에 눈 모양 이미지를 클릭하면 pw-wrapper class 내의 input의 type을 password에서 text로 바꿔주고 눈 모양 이미지를 활 성활된 이미지로 바꿔줍니다.
+    -   현재 input의 type이 text라면 type을 password로 바꿔주고 눈 모양 이미지를 비활성화된 이미지로 바꿔줍니다.
+
 ### 4-2. 어려웠던 점
+
+-   Input을 구현함에 있어서는 이런 로그인을 처음 구현해보는 것이라 로직을 어떻게 할지 명세하는 것이 조금 복잡했었습니다.
 
 <!--
 해야할 것
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 const StyledForm = styled.form`
     display: flex;
     width: 270px;
@@ -74,11 +74,8 @@ const StyledForm = styled.form`
 `;
 
 const Input = () => {
-    // email 관련 Hook
     const [emailValid, setEmailValid] = useState(false);
     const [showAlert, setShowAlert] = useState(false);
-
-    // password 관련 Hook
     const [showPW, setShowPW] = useState(false);
 
     // email 유효성 검사 함수
@@ -90,16 +87,11 @@ const Input = () => {
 
     // email input DOM의 이벤트 핸들러
     const handleChange = (e) => {
-        // js에서는 eventTarget.value였나.. 찾아보기
-
-        // 1. 이메일이 비었다면 알림 끄기
-        // 2. 이메일이 유효해졌다면 체크버튼 활성화
-        // 3. 입력에 의해 이메일이 유효하지 않아지면 체크버튼 비활성화
         if (e.target.value === "") {
             setShowAlert(false);
         } else if (validateEmail(e.target.value)) {
-            setEmailValid(true);
             setShowAlert(false);
+            setEmailValid(true);
         } else if (!validateEmail(e.target.value)) {
             setEmailValid(false);
         }
@@ -113,10 +105,6 @@ const Input = () => {
             e.currentTarget === e.target
         ) {
             setShowAlert(true);
-        }
-        // emailValid가 true일 때 포커스 아웃된 경우
-        else if (emailValid && e.currentTarget === e.target) {
-            setShowAlert(false);
         }
     };
 
@@ -175,17 +163,3 @@ const Input = () => {
 };
 
 export default Input;
-
-// email 요구 사항
-// 1. validate === true 면 체크 버튼이 활성화
-// 2. validate === false인 상태로 다음 탭으로 넘어가면 알림 활성화
-// 3. 메일에 입력이 없으면 알림 비활성화
-
-// password 요구 사항
-// 1. 눈 모양 버튼을 클릭하지 않으면 비밀번호 형식으로 표시
-// 2. 눈 모양 버튼을 클릭해서 활성화가되면 입력된 비밀번호 원문이 보이도록 표시
-
-// PureComonent 사용하는 이유에 대해서 한번 생각해보기
-// 추측
-// - setState 함수들이 호출되면 무조건 다시 렌더링되는데 이때 state변화가 없어도 렌더링되기 때문인가?
-// - 렌더링되는 컴포넌트의 하위 컴포넌트들이 강제적으로 렌더링되어서인가?
